cfd8e122110e7744ea2319763c17b011be35f3b4
Onnisut1 <onnisut1@gmail.com>
2024-10-07 12:53:10 +0300

    Added early movement and jumping

--- game.py
+++ game.py
@@ -1,31 +1,71 @@
 #Game!
-
-import pygame 
-
-
-#Näytön asentaminen ja näytön koko
-screen = pygame.display.set_mode((800, 500))
+import pygame, sys
 
 clock = pygame.time.Clock()
 
-background_colour = (135, 206, 235)
+from pygame.locals import *
+pygame.init() 
 
 pygame.display.set_caption('Peli')
-display = pygame.Surface((800,500))
-screen.fill(background_colour)
 
-#Päivittää näytön
-pygame.display.flip()
 
-running = True
+#Näytön asentaminen ja näytön koko
+window_size = (800,400)
+
+screen = pygame.display.set_mode(window_size,0,32)
+
+#Kuvien lisääminen
+player_png = pygame.image.load('Pelaaja.png')
+
+background_colour = (135, 206, 235)
+
+
+player_location = [50,300]
+player_y_momentum = 0
+
+
+moving_left = False
+moving_right = False
+
+player_rect = pygame.Rect(player_location[0],player_location[1],player_png.get_width(),player_png.get_height())
+
 
 #Game loop
-while running:
 
+while True:
+
+    screen.fill(background_colour)
+    screen.blit(player_png, player_location)
+
+
+    if moving_right == True:
+        player_location[0] += 4
+    if moving_left == True:
+        player_location[0] -= 4
+
+    if player_location[1] > window_size[1]-player_png.get_height():
+        player_y_momentum = -player_y_momentum
+    else:
+        player_y_momentum += 0.2
+    player_location[1] += player_y_momentum
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                moving_right = True
+            if event.key == K_LEFT:
+                moving_left = True
+            if event.key == K_SPACE:
+                    player_y_momentum = -5
+        if event.type == pygame.KEYUP:
+            if event.key == K_RIGHT:
+                moving_right = False
+            if event.key == K_LEFT:
+                moving_left = False
 
+    pygame.display.update()
     #60 Fps
     clock.tick(60)
